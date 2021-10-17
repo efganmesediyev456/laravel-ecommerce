b17b481826660153510f297125ba4f533165752a
efgan <efqanesc@gmail.com>
2021-10-17 22:06:08 +0400

    forgot password with email

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -1,36 +1,51 @@
+
+
+
+
+
 <x-guest-layout>
-    <x-jet-authentication-card>
-        <x-slot name="logo">
-            <x-jet-authentication-card-logo />
-        </x-slot>
 
-        <x-jet-validation-errors class="mb-4" />
+<main id="main" class="main-site left-sidebar">
 
-        <form method="POST" action="{{ route('password.update') }}">
-            @csrf
+		<div class="container">
 
-            <input type="hidden" name="token" value="{{ $request->route('token') }}">
+			<div class="wrap-breadcrumb">
+				<ul>
+					<li class="item-link"><a href="#" class="link">home</a></li>
+					<li class="item-link"><span>login</span></li>
+				</ul>
+			</div>
+			<div class="row">
+				<div class="col-lg-6 col-sm-6 col-md-6 col-xs-12 col-md-offset-3">
+					<div class=" main-content-area">
+						<div class="wrap-login-item ">						
+							<div class="login-form form-item form-stl">
+                            <x-jet-validation-errors class="mb-4" />
+								<form name="frm-login" action="{{ route('password.update') }}" method="POST">
+                                @csrf
+                                    <input type="hidden" name="token" value="{{ $request->route('token') }}">
+									<fieldset class="wrap-input">
+										<label for="frm-login-uname">Email Address:</label>
+										<input id="frm-login-uname" type="email" name="email" value="{{$request->email}}" required autofocus>
+									</fieldset>
+									<fieldset class="wrap-input">
+										<label for="frm-login-pass">Password:</label>
+										<input  id="frm-login-pass" type="password" name="password" required autocomplete="new-password">
+									</fieldset>
+                                    <fieldset class="wrap-input">
+										<label for="frm-login-pass">{{ __('Confirm Password') }}:</label>
+										<input  id="frm-login-pass" type="password" name="password_confirmation" required autocomplete="new-password">
+									</fieldset>
+									<input type="submit" class="btn btn-submit" value="Login" name="submit">
+								</form>
+							</div>												
+						</div>
+					</div><!--end main products area-->		
+				</div>
+			</div><!--end row-->
 
-            <div class="block">
-                <x-jet-label for="email" value="{{ __('Email') }}" />
-                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email', $request->email)" required autofocus />
-            </div>
+		</div><!--end container-->
 
-            <div class="mt-4">
-                <x-jet-label for="password" value="{{ __('Password') }}" />
-                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
-            </div>
+	</main>
 
-            <div class="mt-4">
-                <x-jet-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
-                <x-jet-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <x-jet-button>
-                    {{ __('Reset Password') }}
-                </x-jet-button>
-            </div>
-        </form>
-    </x-jet-authentication-card>
-</x-guest-layout>
+    </x-guest-layout>
